aa85b1a0e35ba1647360dbbc8950a96a9a78f7f5
Paulo Barcelos <paulo@azucrina.org>
2014-12-01 17:31:47 +0100

    moved to demo

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 var static = require('node-static');
 var file = new static.Server('./public', {cache:0});
 
+var instalations = [
+	'nordictable',
+	'test'
+]
 require('http').createServer(function (request, response) {
+	
+	if(request.url == '/'){
+		response.writeHead(302,	{Location: instalations[0] + '/station'});
+		response.end();
+		return;
+	}
+	
 	if(request.url.substr(request.url.length - 1) == '/'){
 		response.writeHead(302,	{
 			Location: request.url.substring(0, request.url.length - 1)
@@ -9,16 +20,14 @@ require('http').createServer(function (request, response) {
 		response.end();
 		return;
 	}
-	if(request.url == '/'){
-		response.writeHead(302,	{Location: 'test/station'});
-		response.end();
-		return;
-	}
-	if(request.url == '/nordic'){
-		response.writeHead(302,	{Location: 'nordic/station'});
-		response.end();
-		return;
-	}
+	for (var i = 0; i < instalations.length; i++) {
+		instalations[i]
+		if(request.url == '/'+instalations[i]){
+			response.writeHead(302,	{Location: instalations[i] + '/station'});
+			response.end();
+			return;
+		}
+	};
 	
 	request.addListener('end', function () {
 		file.serve(request, response);
